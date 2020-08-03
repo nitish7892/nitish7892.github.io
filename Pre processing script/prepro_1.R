4ec0ce4ce17a2d6084f6b563b038014234195dbc
nitish7892 <61034728+nitish7892@users.noreply.github.com>
2020-08-03 11:46:09 -0500

    d

--- Pre processing script/prepro_1.R
+++ Pre processing script/prepro_1.R
@@ -10,9 +10,9 @@ colnames(genre_groupings) <- c("Genre","NA_Sales",
                                "Total")
 genre_groupings <- genre_groupings[order(-genre_groupings$Total),]
 
-genre_groupings <- genre_groupings[1:10,-6]
+genre_groupings <- genre_groupings[-6]
 
-write.csv(genre_groupings,"Genre_Overallgroupings.csv",row.names = F)
+write.csv(genre_groupings,"Genre_Over_group.csv",row.names = F)
 
 
 genre_groupings <- data %>% group_by(Publisher) %>% summarise(sum(NA_Sales),sum(EU_Sales),sum(JP_Sales),sum(Other_Sales),sum(Global_Sales))
@@ -27,7 +27,7 @@ genre_groupings <- genre_groupings[order(-genre_groupings$Total),]
 genre_groupings <- genre_groupings[1:10,-6]
 
 
-write.csv(genre_groupings,"Publisher_Overallgroupings_top5.csv",row.names = F)
+write.csv(genre_groupings,"Publisher_Overallgroupings_top10.csv",row.names = F)
 
 
 
@@ -43,7 +43,7 @@ genre_groupings <- genre_groupings[order(-genre_groupings$Total),]
 genre_groupings <- genre_groupings[1:10,-6]
 
 
-write.csv(genre_groupings,"Platform_Overallgroupings_top5.csv",row.names = F)
+write.csv(genre_groupings,"Platform_Overallgroupings_top10.csv",row.names = F)
 
 
 
